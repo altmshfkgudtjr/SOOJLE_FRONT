4e6f5d1bfa3e98f329862abd8c9e8485d2d0af22
NB <altmshfkgudtjr@naver.com>
2019-10-06 02:15:13 +0900

    main page 1 start done

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,8 @@
-/*Slowly Scroll*/
+// 메인페이지 화면 이동 버튼 위치 setting
+function set_introduce_butotn_position() {
+	let browser_height = $(window).height();
+	let browser_width = $(window).width();
+	$("#SJ_main_introduce_button").css({"top": browser_height - 50, "left": browser_width / 2 - 25});	
+}
+$(window).ready(function() {set_introduce_butotn_position();});
+$(window).resize(function() {set_introduce_butotn_position();});
