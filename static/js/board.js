c3dd4efa7e93750d4f1678fc0b48ea216a14a487
NB <altmshfkgudtjr@naver.com>
2019-10-30 20:15:36 +0900

    search bar pc version done

--- static/js/board.js
+++ static/js/board.js
@@ -52,14 +52,59 @@ function Goboard() {
 	window.location.href = "/board";
 }
 
-function search_focus() {
+let search_cache = "";	// 이전 검색어
+let search_target = "";	// 목표 검색어
+let now = 0;	// 현재 화살표로 선택한 div 위치
+let all = 0;	// 검색결과 수
+function search_focus(keyCode) {
+	//KeyUp 38 or KeyDown 40
+	if (keyCode == 13) {
+		search_button();
+		search_blur();
+	} else if (keyCode == 38 || keyCode == 40) {
+		$(`.search_result:nth-child(${now})`).removeClass("search_target");
+		if (keyCode == 38) now--;
+		else now++;
+		if (now > all) {
+			now--;
+		} else if (now <= 0) {
+			now++;
+		}
+		$(`.search_result:nth-child(${now})`).addClass("search_target");
+		let target = $(`.search_result:nth-child(${now})`).text().trim();
+		$("#pc_search_input").val(target);
+		search_cache = target;
+	} else {
+		let now_search = $("#pc_search_input").val();
+		// 문열길이!=0, 문자열변화
+		if (now_search.length != 0 && search_cache != now_search) {
+			$(`.search_result:nth-child(${now})`).removeClass("search_target");
+			now = 0;
+			search_cache = $("#pc_search_input").val();
+			search_target = search_cache;
+			$("#pc_search_recommend_box").removeClass("display_none");
+			/*추천검색어 AJAX 요청 공간*/
+			all = 3;	// AJAX로 요청한 추천검색우 수
+		} else if ($("#pc_search_input").val() == "") {
+			search_target = "";
+			$("#pc_search_recommend_box").addClass("display_none");
+			$("#pc_search_recommend_box").empty();
+			let line = '<div id="search_loading" class="search_loading pointer noselect">\
+							<i class="fas fa-grip-lines"></i>\
+						</div>';
+			document.getElementById('pc_search_recommend_box').innerHTML = line;
+			search_cache = "";
+		}
+	}
+}
+function search_click() {
 	$("#pc_search_recommend_box").removeClass("display_none");
-	if ($("#pc_search_input").val() == "")
-		$("#pc_search_recommend_box").addClass("display_none");
-	console.log($("#pc_search_input").val());
 }
 function search_blur() {
 	$("#pc_search_recommend_box").addClass("display_none");
+}
+function search_button() {
+	/*search 클릭 작업*/
 }
 
 let grid_open = 0;
