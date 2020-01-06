dfb93f80c4238606ae49bef5534c809584ab908a
NB <altmshfkgudtjr@naver.com>
2020-01-06 19:22:22 +0900

    writing posts done

--- static/js/manager.js
+++ static/js/manager.js
@@ -98,6 +98,50 @@ function cancel_blacklist() {
 
 // 게시글 작성 Div 생성
 function insert_wrting_div() {
-	let target = $("#setting_writing_post_wrapper");
+	let pharagh_placeholder = 
+`\
+내용을 입력해주세요.
 
+만약 공지사항으로 올리고 싶으면 아래 공지사항 버튼을 클릭해주세요.
+게시글을 올릴 시, 다음 규칙을 확인하고 올려주세요.
+
+[게시글 작성 유의사항]
+1. 욕설, 비하, 음란물, 개인정보가 포함된 게시물 게시.
+2. 특정인이나 단체/지역을 비방하는 행위.
+3. 기타 현행법에 어긋나는 행위.
+`;
+	let target = $("#setting_writing_post_wrapper");
+	let div =	`<input type="text" id="setting_writing_post_title" class="setting_writing_post_title" placeholder="제목을 입력해주세요.">
+				<textarea id="setting_writing_post_pharagh" class="setting_writing_post_pharagh" placeholder="${pharagh_placeholder}"></textarea>
+				<input type="checkbox" id="post_notice_checkbox" name="post_notice_checkbox">
+				<div class="setting_toggle">
+					<label for="post_notice_checkbox"></label>
+				</div>
+				<div class="setting_writing_post_checkbox_info noselect">공지사항</div>
+				<div class="setting_writing_post_btn_ok pointer" onclick="writing_post_admin()">작성하기</div>
+				`;
+	target.append(div);
+}
+function writing_post_admin() {
+	let title = $("#setting_writing_post_title").val();
+	if (title == "") {
+		Snackbar("제목을 입력해주세요.");
+		$("#setting_writing_post_title").focus();
+		return;
+	}
+	let pharagh = $("#setting_writing_post_pharagh").val();
+	if (title == "") {
+		Snackbar("내용을 입력해주세요.");
+		$("#setting_writing_post_pharagh").focus();
+		return;
+	}
+	let notice = 0;
+	if($("#post_notice_checkbox").is(":checked")) notice = 1;
+	else notice = 0;
+	let send_data = {
+						"title": title, 
+						"post": pharagh,
+						"notice": notice
+					};
+	// 여기 에이작스
 }
