a56fb359a87092da6dc2de1545a9b981cebae2a9
NB <altmshfkgudtjr@naver.com>
2020-01-08 22:35:40 +0900

    charJS license import

--- static/js/footer.js
+++ static/js/footer.js
@@ -79,11 +79,13 @@ function Insert_license() {
 	window.scrollTo(0,0);
 
 	let div = 	`<div class="lab_title_wrap">
-					<div class="lab_title noselect">라이센스 표기</div>
+					<div class="lab_title noselect">라이센스</div>
 					<div class="lab_subtitle noselect">
 						다음은 SOOJLE의 중요한 부분을 담당하는 소스코드의 라이센스입니다.
 					</div>
-				</div>`;
+				</div>
+				<div class="license_block">${license_1}</div>
+				`;
 	$("#posts_target").append(div);
 	$("#posts_creating_loading").addClass("display_none");
 }
@@ -131,3 +133,17 @@ function Insert_privacy() {
 	$("#posts_target").append(div);
 	$("#posts_creating_loading").addClass("display_none");
 }
+
+
+
+
+
+let license_1 = `The MIT License (MIT)
+<br>
+Copyright (c) 2018 Chart.js Contributors
+<br>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+<br>
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+<br>
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
